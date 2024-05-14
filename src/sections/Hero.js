5b606a346edf1467e92c98daae95ff83d89f7a4f
Travis-Ashcraft <132078620+Travis-Ashcraft@users.noreply.github.com>
2024-05-14 15:08:00 -0500

    Added a better video

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,20 +1,38 @@
-﻿import React from 'react';
+﻿import React, { useRef, useEffect } from 'react';
 import '../styles/Hero.css';  // Adjust path if necessary
 
 const Hero = () => {
+    const videoRef = useRef(null);
 
+    useEffect(() => {
+        const videoElement = videoRef.current;
+        if (videoElement) {
+            const handleVideoLoop = () => {
+                videoElement.style.opacity = '0';
+                setTimeout(() => {
+                    videoElement.style.opacity = '1';
+                    videoElement.currentTime = 0; // Reset video to the start
+                    videoElement.play(); // Ensure it plays again smoothly
+                }, 1000); // Matches the transition time in CSS
+            };
+
+            videoElement.addEventListener('ended', handleVideoLoop);
+            return () => {
+                videoElement.removeEventListener('ended', handleVideoLoop);
+            };
+        }
+    }, []);
 
     return (
         <div className="hero">
-            <div className="imageBackground">
-                <img src={process.env.PUBLIC_URL + "/images/OpenDoor.jpeg"} alt="TSTC Playhub Logo"/>
-            </div>
+            <video autoPlay muted ref={videoRef} className="videoBackground">
+                <source src={process.env.PUBLIC_URL + "/videos/new_HMD.mp4"} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className="content">
-                <div className="contentBox">
-                    <h1>Welcome to TSTC Playhub</h1>
-                    <p>Your gateway to integrating fun and learning!</p>
-                    <button className="ctaButton">Get Started</button>
-                </div>
+                <h1>Welcome to TSTC Playhub</h1>
+                <p>Your gateway to integrating fun and learning!</p>
+                <button className="ctaButton">Get Started</button>
             </div>
         </div>
     );
