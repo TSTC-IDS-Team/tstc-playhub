1f28f043dc10a8c81a7123789373f29f8cf10d5c
Travis-Ashcraft <132078620+Travis-Ashcraft@users.noreply.github.com>
2024-05-14 14:02:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <Router >
             <div className="App">
-                <Home/>
+                <Header/>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/faq" element={<FAQ />} />
